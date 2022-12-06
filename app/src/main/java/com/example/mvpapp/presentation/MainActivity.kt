449c0e5f9187f7a37ed1e5a83f9d4ec09929ed6e
newdir <dimarezanov1@gmail.com>
2022-12-06 09:01:17 +0300

    Changed getShopList to liveData.

--- app/src/main/java/com/example/mvpapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mvpapp/presentation/MainActivity.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvpapp.R
-import com.example.mvpapp.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
-    private  lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
             Log.d("MainActivity", it.toString())
+            if  (count == 0){
+                val item = it[0]
+                viewModel.changeEnableState(item)
+                count++
+            }
         }
-        viewModel.getShopList()
     }
 }
