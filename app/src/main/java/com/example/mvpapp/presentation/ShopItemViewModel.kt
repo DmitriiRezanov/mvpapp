0e9f52de18d95f5cf1eeaaa6ca08efec06d79a84
newdir <dimarezanov1@gmail.com>
2022-12-22 09:16:55 +0300

    Edit ShopItemViewModel.kt

--- app/src/main/java/com/example/mvpapp/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/mvpapp/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.mvpapp.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mvpapp.data.ShopListRepositoryImpl
 import com.example.mvpapp.domain.AddShopItemUseCase
@@ -7,60 +9,96 @@ import com.example.mvpapp.domain.EditShopItemUseCase
 import com.example.mvpapp.domain.GetShopItemUseCase
 import com.example.mvpapp.domain.ShopItem
 
-class ShopItemViewModel : ViewModel(){
+class ShopItemViewModel : ViewModel() {
 
-    private  val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl
 
     private val getShopItemUsesCase = GetShopItemUseCase(repository)
     private val addShopItemUsesCase = AddShopItemUseCase(repository)
     private val editShopItemUsesCase = EditShopItemUseCase(repository)
 
-    fun getShopItem(shopItemId : Int) {
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+    fun getShopItem(shopItemId: Int) {
         val item = getShopItemUsesCase.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
-    fun addShopItem (inputName: String?, inputCount: String?) {
+    fun addShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
-        val fieldsValid = validateInput(name,count)
+        val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
-            val shopItem = ShopItem(name,count, true)
+            val shopItem = ShopItem(name, count, true)
             addShopItemUsesCase.addShopItem(shopItem)
+            finishWork()
         }
+
     }
 
-    fun editShopItem (inputName: String?, inputCount: String?) {
+    fun editShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
-        val fieldsValid = validateInput(name,count)
+        val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
-            val shopItem = ShopItem(name,count, true)
-            editShopItemUsesCase.editShopItem(shopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUsesCase.editShopItem(item)
+                finishWork()
+            }
+
         }
+
     }
 
-    private fun parseName(inputName : String?) : String {
+    private fun parseName(inputName: String?): String {
         return inputName?.trim() ?: ""
     }
 
-    private fun parseCount(inputCount: String?): Int{
+    private fun parseCount(inputCount: String?): Int {
         return try {
             inputCount?.trim()?.toInt() ?: 0
-        } catch (e : Exception){
+        } catch (e: Exception) {
             0
         }
     }
 
     private fun validateInput(name: String, count: Int): Boolean {
         var result = true
-        if (name.isBlank()){
-            // TODO: show error input name
+        if (name.isBlank()) {
+            _errorInputName.value = true
             result = false
         }
-        if (count <= 0){
-            // TODO: show error input count
+        if (count <= 0) {
+            _errorInputCount.value = true
             result = false
         }
         return result
+    }
+
+    public fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    public fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 }
