eaf7d497a0f1b40966163b1076912b9e9128992b
newdir <dimarezanov1@gmail.com>
2023-03-13 22:06:03 +0300

    Used MediatorLiveData, Coroutine Scope and Dispatchers.

--- app/src/main/java/com/example/mvpapp/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/mvpapp/data/ShopListRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.mvpapp.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.mvpapp.domain.ShopItem
 import com.example.mvpapp.domain.ShopListRepository
@@ -14,25 +15,27 @@ class ShopListRepositoryImpl(
     private val shopListDao = AppDatabase.getInstance(application).shopListDao()
     private val mapper = ShopListMapper()
 
-    override fun addShopItem(shopItem: ShopItem) {
+    override suspend fun addShopItem(shopItem: ShopItem) {
         shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun deleteShopItem(shopItem: ShopItem) {
+    override suspend fun deleteShopItem(shopItem: ShopItem) {
         shopListDao.deleteShopItem(shopItem.id)
     }
 
-    override fun editShopItem(shopItem: ShopItem) {
+    override suspend fun editShopItem(shopItem: ShopItem) {
         shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun getShopItem(shopItemId: Int): ShopItem {
+    override suspend fun getShopItem(shopItemId: Int): ShopItem {
         val dbModel = shopListDao.getShopItem(shopItemId)
         return mapper.mapDbModelToEntity(dbModel)
     }
 
-    override fun getShopList(): LiveData<List<ShopItem>> {
-        return shopListLiveData
+    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
+        addSource(shopListDao.getShopList()){
+            value = mapper.mapListDbModelToListEntity(it)
+        }
     }
 
 }
